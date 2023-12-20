ea7b8a7d0a895f7cd1202f92bca37900e444451a
Somtochukwu Nnaji <lyon4197@gmail.com>
2023-12-20 20:43:05 +0100

    About to work on the sign in

--- src/Components/Global/Button/Button.tsx
+++ src/Components/Global/Button/Button.tsx
@@ -8,7 +8,7 @@ export const Button = ({
 }: ButtonProp) => {
 	return (
 		<button
-			className={`text-light flex items-center space-x-1 justify-center w-40 h-10  rounded-[5px] ${className}`}
+			className={`text-light flex items-center space-x-1 justify-center w-40 h-10  rounded-[5px] active:scale-95 ${className}`}
 			onClick={onClick}>
 			{element}
 			<p className="lg:text-sm font-semibold capitalize text-xs">{caption}</p>
--- src/Components/PageContent/PageContent.tsx
+++ src/Components/PageContent/PageContent.tsx
@@ -3,11 +3,10 @@ import { AppRoutes } from "../AppRoutes";
 
 export const PageContent = () => {
 	return (
-		<div className="rounded-t-xl bg-[#fff] w-full shadow h-full pt-10 overflow-auto no-scroll">
-			<div className="max-w-5xl m-auto pt-20 lg:pt-0 px-5">
+		<div className="w-full bg-[#fff] h-full lg:overflow-auto pt-[86px] lg:pt-10 lg:rounded-t-xl lg:shadow no-scroll">
+			<div className="max-w-5xl m-auto px-5">
 				<AppRoutes />
 			</div>
 		</div>
 	);
 };
-
